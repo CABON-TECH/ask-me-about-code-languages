22bc784b666aa910de63fee7eda46c305d208f04
CABON-TECH <kishushubonifacembogho@gmail.com>
2023-11-28 23:07:42 +0300

    connecting the database

--- backend/node-service/server.js
+++ backend/node-service/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const path = require('path');
+const connectDB = require('./src/config/config');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+
+connectDB();
 
 app.use(express.json());
 
---
+++ backend/node-service/src/config/config.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://kishushubonifacembogho:cabon12345@cabon.mo5f37c.mongodb.net/ask-about-coding-languages?retryWrites=true&w=majority';
+
+
+
+
+
+
+
+
+
+
+
+
+/*const conn = await mongoose.connect(process.env.MONGODB_URI, 
+	{ useNewUrlParser: true, 
+		useUnifiedTopology: true 
+	})
+  .then(() => {
+    console.log(`Connected to MongoDB: ${conn.connection.host}`.cyan.underline.bold));
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });*/
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(MONGODB_URI)
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
+    }
+    catch (err) {
+        console.error(err)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
